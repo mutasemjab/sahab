1a9189ebbf8c0a7cc1895026a90719c77d849c69
mutasem <mutasemms198@gmail.com>
2025-09-21 22:34:05 +0300

    first commit

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -10,11 +10,5 @@ class Setting extends Model
     use HasFactory;
     protected $guarded = [];
 
-    protected static function booted()
-    {
-        static::updated(function ($setting) {
-            \Cache::forget('site.setting');
-        });
-    }
 
 }
